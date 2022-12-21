97c9731c32643ce81720b1e54f0cfcb7167a1a9e
Galitan-dev <galitan.dev@gmail.com>
2022-12-21 10:09:44 +0100

    chore: moves main functions from bin to lib

--- src/bin/rocky.rs
+++ src/bin/rocky.rs
@@ -1,14 +1,8 @@
 extern crate rocky;
 
 use clap::{parser::RawValues, ArgMatches};
-use rocky::{
-    assembler::Assembler,
-    cli::cli,
-    repl::{REPLMode, REPL},
-    vm::VM,
-};
+use rocky::{cli::cli, repl::REPLMode, run_file, start_repl};
 use rustyline::error::ReadlineError;
-use std::{fs::File, io::Read, path::Path};
 
 fn main() -> Result<(), ReadlineError> {
     let matches = cli().get_matches();
@@ -21,60 +15,6 @@ fn main() -> Result<(), ReadlineError> {
     }
 
     Ok(())
-}
-
-fn start_repl(mode: REPLMode) -> Result<(), ReadlineError> {
-    let mut repl = REPL::new(mode)?;
-    repl.run();
-    Ok(())
-}
-
-fn read_file(tmp: &str) -> String {
-    let filename = Path::new(tmp);
-    match File::open(Path::new(&filename)) {
-        Ok(mut fh) => {
-            let mut contents = String::new();
-            match fh.read_to_string(&mut contents) {
-                Ok(_) => {
-                    return contents;
-                }
-                Err(e) => {
-                    println!("There was an error reading file: {:?}", e);
-                    std::process::exit(1);
-                }
-            }
-        }
-        Err(e) => {
-            println!("File not found: {:?}", e);
-            std::process::exit(1)
-        }
-    }
-}
-
-fn run_file(num_threads: usize, filename: &str) {
-    let program = read_file(filename);
-    let mut asm = Assembler::new();
-    let mut vm = VM::new();
-    vm.logical_cores = num_threads;
-    let program = asm.assemble(&program);
-    match program {
-        Ok(p) => {
-            vm.add_bytes(p);
-            let events = vm.run();
-            println!("VM Events");
-            println!("--------------------------");
-            for event in &events {
-                println!("{:#?}", event);
-            }
-            std::process::exit(0);
-        }
-        Err(errors) => {
-            println!("Encountered {} assembler error(s):", errors.len());
-            for error in errors {
-                println!("{error}");
-            }
-        }
-    }
 }
 
 fn get_arguments<'a>(matches: &'a ArgMatches) -> Args<'a> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+use std::{fs::File, io::Read, path::Path};
+
+use assembler::Assembler;
+use repl::{REPLMode, REPL};
+use rustyline::error::ReadlineError;
+use vm::VM;
+
 extern crate chrono;
 extern crate clap;
 extern crate colored;
@@ -17,3 +24,57 @@ pub mod instruction;
 pub mod repl;
 pub mod scheduler;
 pub mod vm;
+
+pub fn start_repl(mode: REPLMode) -> Result<(), ReadlineError> {
+    let mut repl = REPL::new(mode)?;
+    repl.run();
+    Ok(())
+}
+
+fn read_file(tmp: &str) -> String {
+    let filename = Path::new(tmp);
+    match File::open(Path::new(&filename)) {
+        Ok(mut fh) => {
+            let mut contents = String::new();
+            match fh.read_to_string(&mut contents) {
+                Ok(_) => {
+                    return contents;
+                }
+                Err(e) => {
+                    println!("There was an error reading file: {:?}", e);
+                    std::process::exit(1);
+                }
+            }
+        }
+        Err(e) => {
+            println!("File not found: {:?}", e);
+            std::process::exit(1)
+        }
+    }
+}
+
+pub fn run_file(num_threads: usize, filename: &str) {
+    let program = read_file(filename);
+    let mut asm = Assembler::new();
+    let mut vm = VM::new();
+    vm.logical_cores = num_threads;
+    let program = asm.assemble(&program);
+    match program {
+        Ok(p) => {
+            vm.add_bytes(p);
+            let events = vm.run();
+            println!("VM Events");
+            println!("--------------------------");
+            for event in &events {
+                println!("{:#?}", event);
+            }
+            std::process::exit(0);
+        }
+        Err(errors) => {
+            println!("Encountered {} assembler error(s):", errors.len());
+            for error in errors {
+                println!("{error}");
+            }
+        }
+    }
+}
